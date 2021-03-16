14a8ab47a4fa55ddcc13aa9a19ddd985dca1b5e1
JoanneTsai <chiaoan.tsai1208@gmail.com>
2021-03-16 09:55:30 +0100

    First practice - connect function with button

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -13,7 +13,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    //create an action
+    @IBAction func showMessage(sender:UIButton){
+        var birthdayDict = ["Mommy":"1/22", "Daddy":"3/25","Ryan":"7/8","Robin":"5/14"]
+        
+//        let alertController = UIAlertController(title: "Welcome to my first app", message: "hello world", preferredStyle: UIAlertController.Style.alert)
+//        //alert style display modally for the app
+//
+//        alertController.addAction(UIAlertAction.init(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+//        present(alertController, animated: true, completion: nil)
+//        //after clicking "OK," the popup message will disappear
+        
+        let selectedButton = sender
+        
+    }
+    //connect the action with the button through simply control-drag the button to view controller icon
+    
 }
 
